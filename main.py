d5d2aa605f34029c20c8eadf51e192f504b2d0b3
atrox3d <atrox3d@gmail.com>
2021-02-09 07:51:02 +0100

    add is_resource_sufficient, process_coins, is_transaction_succesful

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 import data
 
+
+def is_resource_sufficient(order_ingredients):
+    """return True when order can be made, False if ingredients are insufficient"""
+    # print(type(data.resources))
+    # print(data.resources)
+    for resource_name, drink_quantity in order_ingredients.items():
+        if drink_quantity >= data.resources[resource_name]:
+            print(f"Sorry, there is not enough {resource_name}.")
+            return False
+        return True
+
+
+def process_coins():
+    """return the total calculated from the coins inserted"""
+    print("Please insert coins.")
+    total = int(input("How manu quarters?: ")) * 0.25
+    total += int(input("How manu dimes?: ")) * 0.1
+    total += int(input("How manu nickels?: ")) * 0.05
+    total += int(input("How manu pennies?: ")) * 0.01
+    return total
+
+
+def is_transaction_succesful(money_received, drink_cost):
+    """returns true if payment is accepted or false if not sufficient"""
+    if money_received >= drink_cost:
+        return True
+    else:
+        print("Sorry that's not enough money, money refunded")
+        return False
+
+
 profit = 0
 is_on = True
 
@@ -13,6 +44,15 @@ while is_on:
         print(f"Milk:   {data.resources['milk']}ml")
         print(f"Coffee: {data.resources['coffee']}g")
         print(f"Money:  ${profit}")
-
+    else:
+        drink = data.MENU[choice]
+        print(drink)
+        if is_resource_sufficient(drink['ingredients']):
+            payment = process_coins()
+            print(f"{payment}$")
+            if payment >= data.MENU[choice]["cost"]:
+                print("ok")
+            else:
+                print("poor")
 
 
